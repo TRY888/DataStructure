779134ab571bc47be10f0342b43decc7ea0d036d
TRY888 <1332981729@qq.com>
2017-08-24 20:35:21 +0800

    删除没用文件

--- include.h
+++
@@ -1,6 +0,0 @@
-#ifndef _INCLUDE_H
-#define _INCLUDE_H
-
-typedef int DataType;
-
-#endif
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "sqQueue.h"
-
-int main(int argc, char *argv[])
-{
-	testSqQueue();
-	return 0;
-}
